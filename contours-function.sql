101c01dfc91d1b5f8825361b310c2223778ce759
feludwig <fludwig@student.ethz.ch>
2023-10-04 10:36:21 +0200

    add lowzoom filtering
    * and move lang from plpglsql to sql
    * tune the simplification function

--- contours-function.sql
+++ contours-function.sql
@@ -1,5 +1,5 @@
---TODO: everywhere add this simplify_if_needed
 
+--TODO: everywhere add this simplify_if_needed
 CREATE OR REPLACE FUNCTION public.simplify_if_needed(way geometry,z integer)
 RETURNS geometry AS $$
 SELECT (CASE
@@ -13,29 +13,27 @@ $$ LANGUAGE SQL STABLE PARALLEL SAFE;
 CREATE OR REPLACE FUNCTION public.contours_vector(z integer, x integer, y integer)
 RETURNS bytea
 AS $$
-DECLARE
-    result bytea;
-    --bounds_geom geometry;
-BEGIN
-    --SELECT ST_Transform(ST_TileEnvelope(z,x,y) INTO bounds_geom;
-    WITH
-      bounds AS (SELECT ST_Transform(ST_TileEnvelope(z,x,y),4326) AS geom),
-      premvt_contours AS (
-        SELECT
-          height::integer AS height,
-          (height::integer)%10=0 AND (height::integer)%50!=0 AS height10,
-          (height::integer)%50=0 AND (height::integer)%100!=0 AS height50,
-          (height::integer)%100=0 AS height100,
-          simplify_if_needed(ST_AsMVTGeom(way,bounds.geom),z) AS geom
-        FROM public.contours, bounds
-        WHERE ST_Intersects(way,bounds.geom) AND (height::integer)%10=0
-      )
-    SELECT ST_AsMVT(premvt_contours,'heights') FROM premvt_contours
-    INTO result;
-    RETURN result;
-END;
+WITH
+  bounds AS (SELECT ST_Transform(ST_TileEnvelope(z,x,y),4326) AS geom),
+  premvt_contours AS (
+    SELECT
+      height::integer AS height,
+      (height::integer)%10=0 AND (height::integer)%50!=0 AS height10,
+      (height::integer)%50=0 AND (height::integer)%100!=0 AS height50,
+      (height::integer)%100=0 AS height100,
+      simplify_if_needed(ST_AsMVTGeom(way,bounds.geom),z) AS geom
+    FROM public.contours, bounds
+    WHERE ST_Intersects(way,bounds.geom) AND (height::integer)%10=0
+      AND (CASE
+        WHEN z<14 THEN (height::integer)%50=0
+        WHEN z<11 THEN (height::integer)%100=0
+        WHEN z<9 THEN false -- remove everything at lowzooms
+        ELSE true
+      END)
+    )
+SELECT ST_AsMVT(premvt_contours,'heights') FROM premvt_contours;
 $$
-LANGUAGE 'plpgsql'
+LANGUAGE 'sql'
 STABLE
 PARALLEL SAFE;
 

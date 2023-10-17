96484d382263402dc72689afb547b298270fe39d
feludwig <fludwig@student.ethz.ch>
2023-10-17 16:00:35 +0200

    contours adjustments
    * was incorrectly returning height50 and height10 at lowzooms,
      make zoom filtering more explicit
    * add overzoom
    * change expressions to ["get",...] now it works

--- contours-function.sql
+++ contours-function.sql
@@ -1,16 +1,4 @@
 
---TODO: everywhere add this simplify_if_needed
-CREATE OR REPLACE FUNCTION public.simplify_if_needed(way geometry,z integer)
-RETURNS geometry AS $$
-SELECT (CASE
-    WHEN z<= 2 THEN ST_SimplifyPreserveTopology(way,100)
-    WHEN z<= 5 THEN ST_SimplifyPreserveTopology(way, 35)
-    WHEN z<= 7 THEN ST_SimplifyPreserveTopology(way, 15)
-    WHEN z<=12 THEN ST_SimplifyPreserveTopology(way, 10)
-    ELSE way
-  END);
-$$ LANGUAGE SQL STABLE PARALLEL SAFE;
-
 CREATE OR REPLACE FUNCTION public.contours_vector(z integer, x integer, y integer)
 RETURNS bytea
 AS $$
@@ -19,24 +7,25 @@ WITH
   premvt_contours AS (
     SELECT
       height::integer AS height,
+      -- %50!=0 implies %100!=0
       (height::integer)%10=0 AND (height::integer)%50!=0 AS height10,
       (height::integer)%50=0 AND (height::integer)%100!=0 AS height50,
       (height::integer)%100=0 AS height100,
-      simplify_if_needed(ST_AsMVTGeom(way,bounds.geom),z) AS geom
+      ST_AsMVTGeom(way,bounds.geom) AS geom
     FROM public.contours, bounds
-    WHERE ST_Intersects(way,bounds.geom) AND (height::integer)%10=0
-      AND (CASE
-        WHEN z<14 THEN (height::integer)%50=0
-        WHEN z<11 THEN (height::integer)%100=0
-        WHEN z<9 THEN false -- remove everything at lowzooms
-        ELSE true
-      END)
-    )
+    WHERE ST_Intersects(way,bounds.geom)
+      AND (
+        (z>=14 AND (height::integer)%10=0)
+        OR (11<=z AND z<14 AND (height::integer)%50=0)
+        OR (9<=z AND z<11 AND (height::integer)%100=0)
+        -- remove everything at lowzooms
+      )
+  )
 SELECT ST_AsMVT(premvt_contours,'heights') FROM premvt_contours;
 $$
 LANGUAGE 'sql'
 STABLE
 PARALLEL SAFE;
 
-SELECT length(public.contours_vector(16,34303,22938));
+SELECT length(public.contours_vector(15,17151,11469));
 

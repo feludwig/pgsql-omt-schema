437db6ceeb8bda427bbb9ee3db18debe51c87345
feludwig <fludwig@student.ethz.ch>
2023-10-27 10:31:00 +0200

    print stats of generated tiles, zoom range support

--- mktiles.py
+++ mktiles.py
@@ -8,29 +8,68 @@ import threading
 import queue
 printer_lock=threading.Lock()
 
+import run #local
+
 dbaccess,z,x,y,outdir=sys.argv[1:]
 format='pbf'
 
-z=int(z)
 xs=[x]
 ys=[y]
 if x=='*' :
     xs=list(range(2**z))
 if y=='*' :
     ys=list(range(2**z))
+if z.find('-')>=0 :
+    # z range, x and y are specified for starting zoom!
+    zs=list(range(*map(int,z.split('-'))))
+else :
+    zs=[int(z)]
 if x.find('-')>=0 :
     xs=list(range(*map(int,x.split('-'))))
 if y.find('-')>=0 :
     ys=list(range(*map(int,y.split('-'))))
 
 class Writer(threading.Thread) :
-    def __init__(self,c) :
+    def __init__(self,c,zs) :
         threading.Thread.__init__(self)
         self.c=c
         self.finished=False
         self.todo=queue.Queue(100)
-        self.total_written=0
-        self.total_count=0
+        self.total_written={z:0 for z in zs}
+        self.total_count={z:0 for z in zs}
+        self.per_layer_stats={z:{} for z in zs}
+    def add_layer_stats_line(self,z,line:dict) :
+        ln=line['name']
+        if ln not in self.per_layer_stats[z] :
+            self.per_layer_stats[z][ln]=[0]*4
+        self.per_layer_stats[z][ln][0]+=1
+        self.per_layer_stats[z][ln][1]+=line['pcent']
+        self.per_layer_stats[z][ln][2]+=line['bytes']
+        self.per_layer_stats[z][ln][3]+=line['rowcount']
+
+    @classmethod
+    def print_layer_stats(cls,c:psycopg2.extensions.cursor,list_of_self,z) :
+        """ Use database for pg_size_pretty...
+        """
+        def get_size_pretty(d:float) :
+            c.execute(c.mogrify('SELECT pg_size_pretty(%s::numeric);',(d,)))
+            return c.fetchone()[0]
+        per_layer_stats={}
+        #collect all data
+        for i in list_of_self :
+            for k,v in i.per_layer_stats[z].items() :
+                if k not in per_layer_stats :
+                    per_layer_stats[k]=[0]*4
+                for ix in (0,1,2,3) :
+                    per_layer_stats[k][ix]+=v[ix]
+        #print
+        headers=['layer_name','avg_pcent','avg_bytes','avg_rowcount']
+        data=[(k,
+            str(round(v[1]/v[0],1)),
+            get_size_pretty(v[2]/v[0]),str(round(v[3]/v[0])),
+            ) for k,v in per_layer_stats.items()]
+        run.print_table(data,headers)
+
 
     def run(self) :
         while True :
@@ -52,32 +91,61 @@ class Writer(threading.Thread) :
         threading.Thread.join(self)
 
     def process(self,z,x,y) :
-        self.c.execute(self.c.mogrify('SELECT omt_all(%s,%s,%s);',(z,x,y)))
+        success=False
+        for i in range(5) : #try again 5 times
+            try :
+                self.c.execute(
+                    self.c.mogrify('SELECT * FROM omt_all_with_stats(%s,%s,%s);',(z,x,y)))
+                success=True
+                break
+            # InternalError_ when function was redefined while running/did not exist when needed
+            except (psycopg2.errors.InFailedSqlTransaction,psycopg2.errors.InternalError_) :
+                self.c.execute('ABORT;')
+                with printer_lock :
+                    print(f'{z}/{x}/{y}.{format}','FailedSql retrying')
+        if not success :
+            with printer_lock :
+                print(f'{z}/{x}/{y}.{format}','retried 5 times, abandoning')
+            return
+        result=[dict(zip([col.name for col in self.c.description],i)) for i in self.c.fetchall()]
+        for line in result :
+            if line['name']=='ALL' :
+                out_data=line['data']
+            else :
+                self.add_layer_stats_line(z,line)
         if not os.path.exists(f'{outdir}/{z}/{x}') :
-            os.makedirs(f'{outdir}/{z}/{x}')
+            os.makedirs(f'{outdir}/{z}/{x}',exist_ok=True)
         with open(f'{outdir}/{z}/{x}/{y}.{format}','wb') as f:
-            bs_written=f.write(self.c.fetchone()[0])
-        self.total_written+=bs_written
-        self.total_count+=1
+            bs_written=f.write(out_data)
+        self.total_written[z]+=bs_written
+        self.total_count[z]+=1
         with printer_lock :
             print(f'{z}/{x}/{y}.{format}',bs_written,'bytes')
 
 
 access=psycopg2.connect(dbaccess)
-ts=[Writer(access.cursor()) for i in range(5)]
+ts=[Writer(access.cursor(),zs) for i in range(5)]
 
 start=time.time()
 [t.start() for t in ts]
 tix=0
-for x in xs :
-    for y in ys :
-        ts[(tix)%len(ts)].todo.put((z,x,y))
-        tix+=1
+for z in zs :
+    for x in xs :
+        for y in ys :
+            scale=2**(z-zs[0])
+            ts[(tix)%len(ts)].todo.put((z,x*scale,y*scale))
+            tix+=1
 
 [t.join() for t in ts]
 
-total_bytes=sum(t.total_written for t in ts)
-total_count=sum(t.total_count for t in ts)
-print(round(total_bytes*1e-6,2),'MB total')
-print('average tilesize',round(total_bytes/total_count*1e-3,2),'KB,',total_count,'tiles')
+total_z_bytes={z:sum(t.total_written[z] for t in ts) for z in zs}
+total_bytes=sum([v for z,v in total_z_bytes.items()])
+total_z_count={z:sum(t.total_count[z] for t in ts) for z in zs}
+total_count=sum([v for z,v in total_z_count.items()])
+print(round(total_bytes*1e-6,2),'MB total written')
+for z in zs :
+    print(f'z{z:02}:')
+    Writer.print_layer_stats(access.cursor(),ts,z)
+    print('average',round(total_z_bytes[z]/total_z_count[z]*1e-3,2),'KB/tile,',
+        total_z_count[z],'tiles')
 print(round(time.time()-start,1),'seconds')
